470fa57b8698a3c4983cb8f95b7f501dcf0d78b6
Natalikud2 <Reimova091@mail.ru>
2022-11-25 17:55:45 +0800

    Homework

---
+++ logistic/views.py
@@ -0,0 +1,22 @@
+from rest_framework.viewsets import ModelViewSet
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.pagination import LimitOffsetPagination
+
+from logistic.models import Product, Stock
+from logistic.serializers import ProductSerializer, StockSerializer
+
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    # при необходимости добавьте параметры фильтрации
+    search_fields = ['title', 'description']
+
+
+class StockViewSet(ModelViewSet):
+    queryset = Stock.objects.all()
+    serializer_class = StockSerializer
+    # при необходимости добавьте параметры фильтрации
+    filter_backends = (DjangoFilterBackend,)
+    filterset_fields = ['products']
+    search_fields = ['products__title', 'products__description']
